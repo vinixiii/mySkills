6cc6f57328e74ce1a8f1b9983c104ce98f65095a
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2022-01-28 10:19:03 -0300

    feat: add splash screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { Home } from './src/pages/Home';
+import RNBootSplash from "react-native-bootsplash";
 
 export default function App() {
+  useEffect(() => {
+    RNBootSplash.hide({ fade: true });
+  }, []);
+
   return (
     <>
-      <StatusBar barStyle="light-content" />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <Home />
     </>
   );
